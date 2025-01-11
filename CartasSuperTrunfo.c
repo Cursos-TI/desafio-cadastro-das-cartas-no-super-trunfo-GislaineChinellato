019036a69e1c983c40ae10d801f3c1c704aa3fd8
GislaineChinellato <gislainechinellato@gmail.com>
2025-01-11 00:09:41 +0000

    entrada de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,42 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    // Variáveis
+    char estado, codigocarta, nomecidade[40];
+    int populacao, numptosturisticos;
+    float areakm, PIB;
+
+    populacao = 1;
+    numptosturisticos = 1;
+    areakm = 1;
+    PIB = 1;
+
+    // Entrada de dados
+    printf("Digite o estado: \n");
+    scanf ("%s", &estado);
+    printf("Digite o código da carta: \n");
+    scanf ("%3s", &codigocarta);
+    printf("Digite o nome da cidade: \n");
+    scanf ("%s", &nomecidade);
+    printf("Digite a população: \n");
+    scanf ("%d", &populacao);
+    printf("Digite a Área em Km2: \n");
+    scanf ("%f", &areakm);
+    printf("Digite o PIB: \n");
+    scanf ("%f", &PIB);
+    printf("Digite o número de pontos turísticos: \n");
+    scanf ("%d", &numptosturisticos);
+
+    // Exibição dos dados cadastrados
+    printf("\n--- Dados Cadastrados ---\n");
+    printf("Estado: %s\n", estado);
+    printf("Código da carta: %s\n", codigocarta);
+    printf("Nome da cidade: %s\n", nomecidade);
+    printf("População: %d\n", populacao);
+    printf("Área em Km2: %.2f\n", areakm);
+    printf("PIB: %.2f\n", PIB);
+    printf("Número de pontos turísticos: %d\n", numptosturisticos);
+
+
     return 0;
 }
